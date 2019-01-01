3247d17e2ab07cb24fd44c65f40882f0d359ff14
i054564 <saurav.sarkar@sap.com>
2019-01-01 18:27:11 +0530

    Moving the Repository Credentials to a constants file

---
+++ org.saurav.cmisbridge/src/org/saurav/cmisbridge/Constants.java
@@ -0,0 +1,9 @@
+package org.saurav.cmisbridge;
+
+public class Constants {
+
+	public static String REPO_URL ="http://localhost:8080/chemistry-opencmis-server-fileshare/browser";
+	public static String REPO_ID = "test";
+	public static String REPO_USERNAME ="test";
+	public static String REPO_PASSWORD ="test";
+}
